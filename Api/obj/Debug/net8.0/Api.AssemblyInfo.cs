aa44f754dc3ef39589c4bdfcea0c3f7a3bb65a20
MagretBro <magretbro@gmail.com>
2024-05-06 20:19:04 +0700

    css

--- Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0220a330a0dfc0d6322dca2a609665870b25ad54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5840b833e8d2ce38aeffec13557b168e796f8ee7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
